5da559744321c69c6cd5d3df62bb29a815a9b4fa
MaxK0 <maxkoryakov1@gmail.com>
2024-01-28 22:37:10 +0500

    Сделаны dashboard + страница управления категориями

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -18,3 +18,24 @@ function closeNav() {
 
 openNavBtn.addEventListener('click', openNav);
 closenavBtn.addEventListener('click', closeNav);
+
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+function showSidebar() {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'grid';
+}
+
+function hideSidebar() {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'grid';
+    hideSidebarBtn.style.display = 'none';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
