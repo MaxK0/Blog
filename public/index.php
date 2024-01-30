0bc46d84e9043784274e1077c57c4a2c2ca636b8
MaxK0 <maxkoryakov1@gmail.com>
2024-01-30 14:19:03 +0500

    Настроена базовая версия маршрутизации

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+define('APP_PATH', dirname(__DIR__));
 
-dd(['Bay!']);
+require_once APP_PATH . '/vendor/autoload.php';
+
+use App\App;
+
+$app = new App();
+
+$app->run();
