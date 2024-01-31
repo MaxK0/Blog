c5efaa567759dd73af0dd6642b1264f1fe607050
MaxK0 <maxkoryakov1@gmail.com>
2024-01-31 17:46:09 +0500

    Добавлены метод для вытаскивания компонентов и проверки на существование файлов

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -2,10 +2,33 @@
 
 namespace App\Kernel\View;
 
+use App\Kernel\Exceptions\ViewNotFoundException;
+
 class View {
 
     public function page(string $name): void {
-        include_once APP_PATH . "/views/pages/$name.php";
+        $viewPath = APP_PATH . "/views/pages/$name.php";
+
+        if (!file_exists($viewPath)) {
+            throw new ViewNotFoundException("Представление $name не было найдено");
+        }
+
+        extract(['view' => $this]);        
+
+        include_once $viewPath;
     }
     
+    public function component(string $name): void {       
+        $componentPath = APP_PATH . "/views/components/$name.php";
+
+        if (!file_exists($componentPath)) {
+            echo "Компонент $name не был найден";
+            return;
+        }
+
+        extract(['view' => $this]);
+
+        include_once $componentPath;
+    }
+
 }
