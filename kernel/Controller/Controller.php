ebf420ee5d28b9ab73a44d65006b5d387726cc15
MaxK0 <maxkoryakov1@gmail.com>
2024-02-01 20:18:58 +0500

    Реализован Redirect

--- kernel/Controller/Controller.php
+++ kernel/Controller/Controller.php
@@ -4,11 +4,13 @@ namespace App\Kernel\Controller;
 
 use App\Kernel\View\View;
 use App\Kernel\Http\Request;
+use App\Kernel\Http\Redirect;
 
 abstract class Controller {
 
     private View $view;
     private Request $request;
+    private Redirect $redirect;
 
     public function view(string $name): void {
         $this->view->page($name);
@@ -23,6 +25,14 @@ abstract class Controller {
     }
 
     public function setRequest(Request $request): void {
+        $this->request = $request;
+    }
+
+    public function redirect(string $url): void {
+        $this->redirect->to($url);
+    }
+
+    public function setRedirect(Request $request): void {
         $this->request = $request;
     }
 
